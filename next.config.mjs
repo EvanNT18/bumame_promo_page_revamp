02689b21e8a45750e0f215d396725b4b3292bbbf
Ilham Bachtiar Irfani <irfanswag94@gmail.com>
2025-06-02 10:32:44 +0700

    feat: add Docker configuration and environment variable for API URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     images: {
         remotePatterns: [
             // !REMOVE THE HTTP ONE ON PROD
@@ -12,6 +13,9 @@ const nextConfig = {
                 hostname: "*",
             },
         ],
+    },
+    env: {
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://bumame-promo-cms-service-994213873341.asia-southeast2.run.app',
     },
     eslint: {
         ignoreDuringBuilds: true,
